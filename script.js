e89c7699ea2a76997f61cde12ddadc9d505f1f0e
messy_viper <wakirithomaitho100@gmail.com>
2021-12-26 17:41:49 +0300

    Creating and filling arrays

--- script.js
+++ script.js
@@ -351,3 +351,39 @@ const acc1Movements = account1.movements;
 //   if ( a > b ) return -1;
 //   if ( a < b ) return 1;
 // }));
+
+
+
+//Creating and filling arrays
+ 
+const arryOne = Array( 1, 2, 3, 4, 5, 6, 7 );
+console.log( arryOne ); //[1, 2, 3, 4, 5, 6, 7]
+
+const arrConstructor = new Array( 7 );
+console.log( arrConstructor ); //[empty × 7]
+
+const arrFilledAfterConstruction = arrConstructor.fill( 1 );
+console.log( arrFilledAfterConstruction ); //[1, 1, 1, 1, 1, 1, 1]
+
+arrFilledAfterConstruction.fill( 3, 2, 5 ); //[1,1, 3, 3, 3, 1, 1 ]
+console.log( arrFilledAfterConstruction );
+
+
+//Using Arrayfrom
+
+const arryTwo = Array.from( { length: 7 }, ( el, idx ) => {
+  //currently element is in nto available
+  console.log(idx );
+  return idx + 1;
+})
+
+console.log( arryTwo ); //[1, 2, 3, 4, 5, 6, 7]
+
+
+
+const randomDiceRolls = Array.from( { length: 100 }, () => Math.floor( Math.random() * 6 ) + 1 );
+console.log( randomDiceRolls ); //[5, 3, 3, 1, 5, 3, 4, 4, 6, 5, 1, 5, 3, 6, 3, 5, 4, 2, 1, 4, 3, 6, 2, 2, 5, 1, 3, 3, 1, 1, 3, 2, 1, 2, 6, 4, 4, 2, 4, 5, 3, 4, 4, 2, 4, 1, 6, 3, 2, 4, 3, 1, 1, 2, 1, 3, 5, 1, 3, 2, 3, 3, 3, 5, 1, 6, 2, 5, 4, 4, 3, 3, 2, 6, 3, 1, 2, 6, 6, 5, 1, 1, 1, 3, 5, 3, 2, 1, 5, 4, 2, 6, 4, 2, 1, 5, 6, 1, 3, 6]
+
+
+const numberOneToHundred = Array.from( { length: 100 }, ( _, idx ) => idx + 1 );
+console.log(numberOneToHundred); //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
